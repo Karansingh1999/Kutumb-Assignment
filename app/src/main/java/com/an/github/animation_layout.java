b373fbeaf8ca627971d1f814b8eeff2c328d555d
kumarkaransingh <7870453811karan@gmail.com>
2022-01-23 04:17:32 +0530

    Kutumb_Assignment

---
+++ app/src/main/java/com/an/github/animation_layout.java
@@ -0,0 +1,13 @@
+package com.an.github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class animation_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_layout);
+    }
+}
